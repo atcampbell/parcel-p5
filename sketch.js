5badec16a6d6c107231020b27d3565b8ab8d6aa7
atcampbell <adamtoddcampbell@gmail.com>
2020-03-02 23:40:45 +0000

    add css & tidy

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
 const height = window.innerHeight;
 const width = window.innerWidth;
-const boxSize = 20;
 
-class Square {
-    constructor(x, y, height, width) {
-        rect(x, y, height, width);
-    }
-}
-
-const setup = () => {
+function setup() {
     createCanvas(width, height);
     background(40);
 };
